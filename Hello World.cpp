70b6130c04b33b09af9aac8da882530960b2529f
ChrisJacobbs <100603454+ChrisJacobbs@users.noreply.github.com>
2022-02-28 11:14:28 -0500

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,16 @@
+// Chris Jacobs
+// 8/1/2021
+// Hello world
+// Displays the message "Hello World"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    system("pause");
+    return 0;
+}
+
